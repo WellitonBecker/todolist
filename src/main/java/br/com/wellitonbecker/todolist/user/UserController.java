ed8801877d9e7e051f3ecf38ae68ab8adcd985e5
WellitonBecker <welliton.lbecker@gmail.com>
2023-10-11 09:19:46 -0300

    Alterado

--- src/main/java/br/com/wellitonbecker/todolist/user/UserController.java
+++ src/main/java/br/com/wellitonbecker/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.wellitonbecker.todolist.user;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @RestController
 @RequestMapping("/users")
@@ -23,6 +26,9 @@ public class UserController {
       return ResponseEntity.badRequest().body("Usuário já existe");
     }
 
+    var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+    userModel.setPassword(passwordHashred);
+    
     var userCreated = this.userRepository.save(userModel);
     return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
